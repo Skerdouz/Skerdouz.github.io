8d929e476f45ab8150355ad3fedc9087779bff28
Skerdouz <lucasbrahinski60@gmail.com>
2022-11-20 22:51:19 +0100

    oui

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const port = 8080;
+const app = express();
+
+app.use(express.static(__dirname + '/'));
+
+app.listen(port);
+console.log("server started on port " + port);
